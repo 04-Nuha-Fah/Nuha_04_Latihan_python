72063a7758cbbc9864b4cfec40da7973b8ef7bd8
Ardiansyah Nuha Fahreza <ardi.tkjsmekas25@gmail.com>
2026-08-03 09:57:48 +0700

    first commit

---
+++ ganjilgenap.py
@@ -0,0 +1,14 @@
+a = int(input("Angka: "))
+
+if a % 2 == 0:
+    print(f"{a} adalah bilangan GENAP")
+else:
+    print(f"{a} adalah bilangan GANJIL")
+    
+b = int(input("Angka: "))
+
+if b // 2 * 2 == b:
+    print(f"{b} adalah bilangan GENAP")
+else:
+    print(f"{b} adalah bilangan GANJIL")
+    
