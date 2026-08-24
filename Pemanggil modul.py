16ef9df1d401ea359e4d4a433f0c84255654bcc4
04-Nuha-Fah <ardi.tkjsmeksa25@gmail.com>
2026-08-24 17:55:23 +0700

    Add files via upload

---
+++ Pemanggil modul.py
@@ -0,0 +1,8 @@
+#import ganjilgenap
+#ganjilgenap.ganjil_genap(print("Berapa Angkamu"))
+
+#import ganjilgenap
+#ganjilgenap.perkalian(print("Berapa Angkamu"))
+
+import ganjilgenap
+ganjilgenap.pembagian(print("Berapa Angkamu"))
