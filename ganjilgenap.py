0a4fc8043c63ca7557eb422c695f53de21c9da6e
04-Nuha-Fah <ardi.tkjsmeksa25@gmail.com>
2026-08-24 09:26:45 +0700

    Ganjil genap dan perulangan python

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -12,3 +12,17 @@ if b // 2 * 2 == b:
 else:
     print(f"{b} adalah bilangan GANJIL")
     
+#Perulangan Ganjil genap#
+
+while True:
+    
+    a = int(input("Angka: "))
+    if a % 2 == 0:
+        print(f"{a} adalah bilangan GENAP")
+    else:
+        print(f"{a} adalah bilangan GANJIL")
+        
+    if a == 100:
+        break
+    
+print("selesai")
