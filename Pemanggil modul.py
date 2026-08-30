d2aa825e276a619d4b9dc88db7e73f8f9daca5b3
04-Nuha-Fah <ardi.tkjsmeksa25@gmail.com>
2026-08-30 18:56:16 +0700

    Update Pemanggil modul bangun datar

--- Pemanggil modul.py
+++ Pemanggil modul.py
@@ -6,3 +6,12 @@ ganjilgenap.perkalian(print("Berapa Angkamu"))
 
 import ganjilgenap
 ganjilgenap.pembagian(print("Berapa Angkamu"))
+
+import ganjilgenap
+ganjilgenap.hitung_luas_persegi_panjang(print("Hitung Luas Persegi Panjangmu"))
+
+import ganjilgenap
+ganjilgenap.hitung_keliling_persegi_panjang(print("Hitung Keliling Persegi Panjangmu"))
+
+import ganjilgenap
+ganjilgenap.hitung_luas_jajar_genjang(print("Hitung Luas Jajar Genjangmu"))
