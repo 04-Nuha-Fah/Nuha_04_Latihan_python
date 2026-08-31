22537d5d5f64a290ba736a4de7ad02407d8b8959
04-Nuha-Fah <ardi.tkjsmeksa25@gmail.com>
2026-08-31 09:16:10 +0700

    Pemanggil Modul

---
+++ main.py
@@ -0,0 +1,17 @@
+import ModulMath
+ModulMath.ganjil_genap(print("Berapa Angkamu"))
+
+import ModulMath
+ModulMath.perkalian(print("Berapa Angkamu"))
+
+import ModulMath
+ModulMath.pembagian(print("Berapa Angkamu"))
+
+import ModulBangunRuang
+ModulBangunRuang.hitung_luas_persegi_panjang(print("Hitung Luas Persegi Panjangmu"))
+
+import ModulBangunRuang
+ModulBangunRuang.hitung_keliling_persegi_panjang(print("Hitung Keliling Persegi Panjangmu"))
+
+import ModulBangunRuang
+ModulBangunRuang.hitung_luas_jajar_genjang(print("Hitung Luas Jajar Genjangmu"))
