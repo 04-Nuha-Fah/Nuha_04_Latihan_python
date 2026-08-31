75496e16bda12c94b41b3279fe31f1af3bf302df
04-Nuha-Fah <ardi.tkjsmeksa25@gmail.com>
2026-08-31 09:56:50 +0700

    Menu Modul

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import ModulMath
-ModulMath.ganjil_genap(print("Berapa Angkamu"))
+import ModulBangunDatar
 
-import ModulMath
-ModulMath.perkalian(print("Berapa Angkamu"))
-
-import ModulMath
-ModulMath.pembagian(print("Berapa Angkamu"))
-
-import ModulBangunRuang
-ModulBangunRuang.hitung_luas_persegi_panjang(print("Hitung Luas Persegi Panjangmu"))
-
-import ModulBangunRuang
-ModulBangunRuang.hitung_keliling_persegi_panjang(print("Hitung Keliling Persegi Panjangmu"))
-
-import ModulBangunRuang
-ModulBangunRuang.hitung_luas_jajar_genjang(print("Hitung Luas Jajar Genjangmu"))
+print("Menu Modul")
+print("1.Ganjil Genap\n2.Perkalian\n3.Pembagian\n4.Luas Persegi Panjang\n5.Keliling Persegi Panjang\n6.Luas Jajar Genjang")
+print("")
+a = int(input("No = "))
+if a == 1:
+    print(ModulMath.ganjil_genap(print("Ganjil Genap\nMasukkan angkamu")))
+if a == 2:
+    print(ModulMath.perkalian(print("Perkalian\nMasukkan Angkamu")))
+if a == 3:
+    print(ModulMath.pembagian(print("Pembagian\nMasukkan Angkamu")))
+if a == 4:
+    print(ModulBangunDatar.hitung_luas_persegi_panjang(print("Hitung Luas Persegi Panjangmu")))
+if a == 5:
+    print(ModulBangunDatar.hitung_keliling_persegi_panjang(print("Hitung Keliling Persegi Panjangmu")))
+if a == 6:
+    print(ModulBangunDatar.hitung_luas_jajar_genjang(print("Hitung Luas Jajar Genjangmu")))
